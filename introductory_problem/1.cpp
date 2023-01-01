f38df35ce0506562621cd2012119eeb3bdaca24b
Akshay kumar gupta <91423787+akshaygupta0909@users.noreply.github.com>
2023-01-01 08:27:12 +0530

    Update 1.cpp

--- introductory_problem/1.cpp
+++ introductory_problem/1.cpp
@@ -1,2 +1,53 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+#define pb push_back
+#define f first
+#define s second
+#define nl   "\n"
+#define rep(i,j,k) for(ll i=j;i<k;i++)
+ 
+using ll = long long;
+ 
+const int N = 1e5+10;
+constexpr ll INF = 2e18;
+ 
+ 
+ 
+ 
+ 
+void solve(){
+     ll n; cin >> n;
+     cout << n << nl;
+     while(n!=1){
+         if(n%2==0){
+            n /=2;
+         }
+        else {
+            n = (n*3 + 1);
+        }
+        cout << n << nl;
+     }
+ 
+ 
+    return;
+      }
+ 
+ 
+int main(){
+ 
+#ifndef ONLINE_JUDGE
+   freopen("input.txt", "r", stdin);
+   freopen("output.txt", "w", stdout);
+   #endif
+ 
+// int t=1; cin >> t;
+// while(t--){
+   solve();
+ 
+    return 0;
+}
+ 
+ 
+ 
+ 
+ 
